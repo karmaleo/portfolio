3a27cd14b4c1f25d366239fcb620b0403d6fd254
karmaleo <76041651+karmaleo@users.noreply.github.com>
2022-10-27 16:29:20 +0600

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,19 +20,19 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'freedb_portfolio107' );
+define( 'DB_NAME', 'freedb_tenzin' );
 
 /** Database username */
 define( 'DB_USER', 'freedb_karma' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'PqX%ka7tz9&4eK%' );
+define( 'DB_PASSWORD', 'scV7tr43q9&MqaX' );
 
 /** Database hostname */
 define( 'DB_HOST', 'sql.freedb.tech' );
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4_general_ci' );
+define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
